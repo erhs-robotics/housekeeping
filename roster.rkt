41a4aa3b1e0f817ee6919026da1bae365a65d859
daniel watson <ozzloy@gmail.com>
2011-10-10 23:41:14 -0400

    make roster

---
+++ roster.rkt
@@ -0,0 +1,13 @@
+; https://github.com/erhs-53-hackers
+
+(team-members
+  ("adamclaassen" "adam" "student")
+  ("area53liz" "liz" "student")
+  ("bsnsj425" "?" "student")
+  ("erhs-53" "erhs" "github account that owns erhs-53-hackers organization")
+  ("LinuxArchitect" "george" "mentor")
+  ("MarianaR" "mariana" "student")
+  ("MichaelStevens" "mike" "student")
+  ("noobguy" "?" "student?")
+  ("nyaculak" "nick" "student")
+  ("ozzloy" "daniel" "mentor"))
