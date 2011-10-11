546a3699fb5616bbdd57da11fd355adfe48aabf1
daniel watson <ozzloy@gmail.com>
2011-10-11 00:42:39 -0400

    add instructions for setting up new dev box

---
+++ setup-new-dev-box.rkt
@@ -0,0 +1,38 @@
+;dependencies
+;  git
+;   browse to http://help.github.com/set-up-git-redirect
+;     follow the instructions, they're pretty thorough
+;  robotpy
+;    browse to http://www.python.org/download/releases/3.2.2/
+;      scroll down to the 'Download' section
+;      download the appropriate installer
+;    in a terminal
+;      mkdir -p ~/opt
+;      cd ~/opt
+;      git clone https://github.com/robotpy/robotpy.git
+;  java
+;    eclipse
+;      browse to http://firstforge.wpi.edu/sf/go/doc1199?nav=1
+;        download from the link "Java Getting Started Guide"
+;    netbeans?
+;  ucpp
+;    homepage https://github.com/nikitakit/ucpp
+;    dependencies
+;      python2
+;        browse to http://www.python.org/download/releases/2.7.2/
+;          scroll down to the 'Download' section
+;          download the appropriate installer
+;    in a terminal
+;      mkdir -p ~/opt
+;      cd ~/opt
+;      git clone https://github.com/nikitakit/ucpp.git
+;      export PATH="${PATH}:${HOME}/opt/ucpp/ucpp"
+;      echo "PATH=\"\${PATH}:\${HOME}/opt/ucpp/ucpp\"" >> ~/.bashrc
+;      ucpp setup -t 53 # this takes a while
+;    browse to http://firstforge.wpi.edu/sf/go/doc1197?nav=1
+;      download from the link "C++ Getting Started Guide"
+;  labview
+;    2011
+;      grab the install cd from george
+;    2012
+;      ?
