164ca2338ae9d32d4e9593bdb4a306a24f7f7c94
Michael Stevens <mls0.5mv2@gmail.com>
2011-11-24 14:47:50 -0500

    Extended functions

--- roster.rkt
+++ roster.rkt
@@ -2,23 +2,32 @@
 #lang racket
 
 (define hackers
-  '(adamclaassen
-    area53liz
-    blakstar000
-    bsnsj425
-    Chetlanl
-    erhs-53
-    LinuxArchitect
-    Mariana
-    MichaelStevens
-    minnie1266
-    noobguy
-    nyaculak
-    ozzloy
-    ShoutenM
-    YuRArea53
-    Vozw
-    wertmensah11))
-
+  '("adamclaassen"
+    "area53liz"
+    "blakstar000"
+    "bsnsj425"
+    "Chetlanl"
+    "erhs-53"
+    "LinuxArchitect"
+    "Mariana"
+    "MichaelStevens"
+    "minnie1266"
+    "noobguy"
+    "nyaculak"
+    "ozzloy"
+    "ShoutenM"
+    "YuRArea53"
+    "Vozw"
+    "wertmensah11"))
+(define last "blah")
+(define hacker "blah")
 (define (random-hacker)
-  (list-ref hackers (random (length hackers))))
+  (set! hacker (list-ref hackers (random (length hackers))))
+  (if (equal? last hacker)
+      (random-hacker)
+      (printf hacker))
+  (set! last hacker))
+(define (list-random-hacker num)
+    (for ([i num]) (random-hacker) (printf "\n"))
+    )
+  
